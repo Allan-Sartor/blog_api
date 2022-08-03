b98ce264c154fece6245ba3176eb38598f30c563
Allan-Sartor <allansartor@outlook.com>
2022-08-03 16:03:07 -0300

    Refactor: code improvements

--- app/controllers/Api/V1/articles_controller.rb
+++ app/controllers/Api/V1/articles_controller.rb
@@ -1,68 +1,63 @@
-module Api
-  module V1
-    class ArticlesController < ApplicationController
-    include Paginable
+class Api::V1::ArticlesController < ApplicationController
+  before_action :article_find_by_slug, only: [:show, :update, :destroy]
+  include Paginable
 
-      # GET /articles included pagination for articles
-      def index
-        @articles = Article.page(current_page).per(per_page)
+  # GET /articles included pagination for articles
+  def index
+    @articles = Article.page(current_page).per(per_page)
     
-        render json: ArticleSerializer.new(@articles, meta_options).serialized_json
-      end
+    render json: ArticleSerializer.new(@articles, meta_options).serialized_json
+  end
     
-      # GET /articles/slug
-      def show
-        @article = Article.find_by(slug: params[:slug])
-
-        render json: ArticleSerializer.new(@article, options).serialized_json
-      end
-    
-      # POST /articles
-      def create
-        @article = Article.new(article_params)
-    
-        if @article.save
-          render json: ArticleSerializer.new(@article).serialized_json
-        else
-          render json: {error: article.errors.messages }, status: 422
-        end
-      end
-    
-      # PATCH/PUT /articles/slug
-      def update
-        @article = Article.find_by(slug: params[:slug])
-
-        if @article.update(article_params)
-          render json: ArticleSerializer.new(@article).serialized_json
-        else
-          render json: {error: article.errors.messages }, status: 422
-        end
-      end
-    
-      # DELETE /articles/slug
-      def destroy
-        @article = Article.find_by(slug: params[:slug])
-
-        if @article.destroy
-          head :no_content
-        else
-          render json: {error: article.errors.messages }, status: 422
-        end
-      end
-    
-      private
-        # Only allow a list of trusted parameters through.
-        def article_params
-          params.require(:article).permit(:title, :body)
-        end
-
-        def options
-          @options ||= { include: %i[reviews] }
-        end
-
-        def meta_options
-          @meta_options ||= { meta: meta_attributes(@articles) }
-        end
+  # GET /articles/slug
+    def show
+      render json: ArticleSerializer.new(@article, options).serialized_json
     end
+    
+  # POST /articles
+  def create
+    @article = Article.new(article_params)
+    
+    if @article.save
+      render json: ArticleSerializer.new(@article).serialized_json
+    else
+      render json: {error: article.errors.messages }, status: 422
+    end
+  end
+    
+  # PATCH/PUT /articles/slug
+  def update
+    if @article.update(article_params)
+      render json: ArticleSerializer.new(@article).serialized_json
+    else
+      render json: {error: article.errors.messages }, status: 422
+    end
+  end
+    
+  # DELETE /articles/slug
+  def destroy
+    if @article.destroy
+      head :no_content
+    else
+      render json: {error: article.errors.messages }, status: 422
+    end
+  end
+    
+  private
+
+  def article_find_by_slug
+    @article = Article.find_by(slug: params[:slug])
+  end
+
+  def options
+    @options ||= { include: %i[reviews] }
+  end
+  
+  def meta_options
+    @meta_options ||= { meta: meta_attributes(@articles) }
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :body)
   end
 end
--- app/controllers/Api/V1/reviews_controller.rb
+++ app/controllers/Api/V1/reviews_controller.rb
@@ -1,38 +1,35 @@
-module Api
-  module V1
-    class ReviewsController < ApplicationController
-      
-      # reviews/ Create new review for articles
-      def create
-        @review = article.reviews.new(review_params)
-    
-        if @review.save
-          render json: ReviewSerializer.new(@review).serialized_json 
-        else
-          render json: { error: review.errors.messages }, status: 422
-        end
-      end
-      
-      # Review/ id - Delete review article
-      def destroy
-        @review = Review.find(params[:id])
-    
-        if @review.destroy
-          head :no_content
-        else
-          render json: {error: review.errors.messages }, status: 422
-        end
-      end
-    
-      private
-      
-        def article
-          @article ||= Article.find(params[:article_id])
-        end
-      
-        def review_params
-          params.require(:review).permit(:title, :description, :score ,:article_id)
-        end
+class Api::V1:::ReviewsController < ApplicationController
+  before_action :set_article, only: [:create, :destroy]
+
+  # reviews/ Create new review for articles
+  def create
+    @review = @article.reviews.new(review_params)
+
+    if @review.save
+      render json: ReviewSerializer.new(@review).serialized_json 
+    else
+      render json: { error: review.errors.messages }, status: 422
     end
   end
+  
+  # Review/ id - Delete review article
+  def destroy
+    @review = @article.review.find(params[:id])
+
+    if @review.destroy
+      head :no_content
+    else
+      render json: {error: review.errors.messages }, status: 422
+    end
+  end
+
+  private
+  
+    def set_article
+      @article = Article.find(params[:article_id])
+    end
+  
+    def review_params
+      params.require(:review).permit(:title, :description, :score, :article_id)
+    end
 end
